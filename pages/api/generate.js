faa611b16f7cfe6b47267003e18c64d0092119ef
Leandro Schlain <cokinio@gmail.com>
2023-02-02 10:22:06 -0300

    generando frases

---
+++ pages/api/generate.js
@@ -0,0 +1,49 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = 
+`
+quiero que me sugieras 15 afirmaciones para leer por la noche que contradigan mi opinion de que
+\n`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  Quiero amplies las siguientes afirmaciones destacando los valores positivos de las mismas. ${basePromptOutput.text}
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.7,
+		// I also increase max_tokens.
+    max_tokens: 850,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
